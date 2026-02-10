8bbb7f8ee716591dd302a2cffca1b1ea79d4e11b
dignifiedquire <me@dignifiedquire.com>
2026-02-10 22:38:27 +0100

    refactor: migrate RustLibopusBackend to safe API
    - Encoder type changed from *mut OpusEncoder to Box<OpusEncoder>
    - opus_encoder_create uses OpusEncoder::new()
    - CTL dispatch uses typed methods (set_bitrate, set_vbr, etc.)
      instead of VarArgs for known request codes
    - Decoder uses method calls (dec.decode, dec.final_range, dec.reset)
    - opus_encoder/decoder_destroy simplified to drop()
    - Trait interface unchanged (upstream C backend still needs unsafe)

--- unsafe-libopus-tools/src/demo/backend.rs
+++ unsafe-libopus-tools/src/demo/backend.rs
@@ -37,52 +37,165 @@ pub(crate) trait OpusBackendTrait {
 }
 
 mod unsafe_libopus {
+    use unsafe_libopus::varargs::{VarArg, VarArgs};
     use unsafe_libopus::{
-        opus_decode, opus_decoder_ctl_impl, opus_encode, opus_encoder_create,
-        opus_encoder_ctl_impl, opus_encoder_destroy, varargs::VarArgs, OpusDecoder, OpusEncoder,
+        opus_encode, Bitrate, OpusDecoder, OpusEncoder, OPUS_GET_FINAL_RANGE_REQUEST,
+        OPUS_GET_LOOKAHEAD_REQUEST, OPUS_RESET_STATE, OPUS_SET_BANDWIDTH_REQUEST,
+        OPUS_SET_BITRATE_REQUEST, OPUS_SET_COMPLEXITY_REQUEST, OPUS_SET_DTX_REQUEST,
+        OPUS_SET_EXPERT_FRAME_DURATION_REQUEST, OPUS_SET_FORCE_CHANNELS_REQUEST,
+        OPUS_SET_LSB_DEPTH_REQUEST, OPUS_SET_VBR_CONSTRAINT_REQUEST, OPUS_SET_VBR_REQUEST,
     };
 
     pub struct RustLibopusBackend;
 
     impl super::OpusBackendTrait for RustLibopusBackend {
-        type Encoder = *mut OpusEncoder;
+        type Encoder = Box<OpusEncoder>;
         type Decoder = Box<OpusDecoder>;
 
         unsafe fn opus_encoder_create(
             Fs: i32,
             channels: i32,
             application: i32,
-        ) -> Result<*mut OpusEncoder, i32> {
-            let mut error = 0;
-
-            let res = opus_encoder_create(Fs, channels, application, &mut error);
-            if res.is_null() {
-                Err(error)
-            } else {
-                Ok(res)
-            }
+        ) -> Result<Box<OpusEncoder>, i32> {
+            OpusEncoder::new(Fs, channels, application).map(Box::new)
         }
 
         unsafe fn opus_encoder_ctl_impl(
-            &mut st: &mut *mut OpusEncoder,
+            st: &mut Box<OpusEncoder>,
             request: i32,
-            args: VarArgs,
+            mut args: VarArgs,
         ) -> i32 {
-            opus_encoder_ctl_impl(st, request, args)
+            // Dispatch known CTL requests to safe typed methods.
+            // The trait requires VarArgs for compatibility with the upstream C backend,
+            // but the Rust backend can use safe methods directly.
+            match request {
+                OPUS_SET_BITRATE_REQUEST => {
+                    if let [VarArg::I32(val)] = &args.0[..] {
+                        st.set_bitrate(Bitrate::from(*val));
+                        0
+                    } else {
+                        -1
+                    }
+                }
+                OPUS_SET_BANDWIDTH_REQUEST => {
+                    if let [VarArg::I32(val)] = &args.0[..] {
+                        let bw = if *val == -1 {
+                            None
+                        } else {
+                            Some((*val).try_into().map_err(|_| -1i32).unwrap())
+                        };
+                        st.set_bandwidth(bw);
+                        0
+                    } else {
+                        -1
+                    }
+                }
+                OPUS_SET_VBR_REQUEST => {
+                    if let [VarArg::I32(val)] = &args.0[..] {
+                        st.set_vbr(*val != 0);
+                        0
+                    } else {
+                        -1
+                    }
+                }
+                OPUS_SET_VBR_CONSTRAINT_REQUEST => {
+                    if let [VarArg::I32(val)] = &args.0[..] {
+                        st.set_vbr_constraint(*val != 0);
+                        0
+                    } else {
+                        -1
+                    }
+                }
+                OPUS_SET_COMPLEXITY_REQUEST => {
+                    if let [VarArg::I32(val)] = &args.0[..] {
+                        st.set_complexity(*val).map_or_else(|e| e, |_| 0)
+                    } else {
+                        -1
+                    }
+                }
+                OPUS_SET_FORCE_CHANNELS_REQUEST => {
+                    if let [VarArg::I32(val)] = &args.0[..] {
+                        let ch = if *val == -1 {
+                            None
+                        } else {
+                            Some((*val).try_into().map_err(|_| -1i32).unwrap())
+                        };
+                        st.set_force_channels(ch).map_or_else(|e| e, |_| 0)
+                    } else {
+                        -1
+                    }
+                }
+                OPUS_SET_DTX_REQUEST => {
+                    if let [VarArg::I32(val)] = &args.0[..] {
+                        st.set_dtx(*val != 0);
+                        0
+                    } else {
+                        -1
+                    }
+                }
+                OPUS_SET_LSB_DEPTH_REQUEST => {
+                    if let [VarArg::I32(val)] = &args.0[..] {
+                        st.set_lsb_depth(*val).map_or_else(|e| e, |_| 0)
+                    } else {
+                        -1
+                    }
+                }
+                OPUS_SET_EXPERT_FRAME_DURATION_REQUEST => {
+                    if let [VarArg::I32(val)] = &args.0[..] {
+                        let fs = (*val).try_into().map_err(|_| -1i32).unwrap();
+                        st.set_expert_frame_duration(fs);
+                        0
+                    } else {
+                        -1
+                    }
+                }
+                OPUS_GET_LOOKAHEAD_REQUEST => {
+                    if let [VarArg::I32Out(ptr)] = &mut args.0[..] {
+                        **ptr = st.lookahead();
+                        0
+                    } else {
+                        -1
+                    }
+                }
+                OPUS_GET_FINAL_RANGE_REQUEST => {
+                    if let [VarArg::U32Out(ptr)] = &mut args.0[..] {
+                        **ptr = st.final_range();
+                        0
+                    } else {
+                        -1
+                    }
+                }
+                OPUS_RESET_STATE => {
+                    st.reset();
+                    0
+                }
+                // Fall back to the unsafe impl for unknown requests
+                other => ::unsafe_libopus::opus_encoder_ctl_impl(
+                    &mut **st as *mut OpusEncoder,
+                    other,
+                    args,
+                ),
+            }
         }
 
         unsafe fn opus_encode(
-            &mut st: &mut *mut OpusEncoder,
+            st: &mut Box<OpusEncoder>,
             pcm: *const i16,
             analysis_frame_size: i32,
             data: *mut u8,
             max_data_bytes: i32,
         ) -> i32 {
-            opus_encode(st, pcm, analysis_frame_size, data, max_data_bytes)
+            opus_encode(
+                &mut **st as *mut OpusEncoder,
+                pcm,
+                analysis_frame_size,
+                data,
+                max_data_bytes,
+            )
         }
 
-        unsafe fn opus_encoder_destroy(st: *mut OpusEncoder) {
-            opus_encoder_destroy(st)
+        unsafe fn opus_encoder_destroy(st: Box<OpusEncoder>) {
+            drop(st)
         }
 
         unsafe fn opus_decoder_create(Fs: i32, channels: i32) -> Result<Box<OpusDecoder>, i32> {
@@ -97,17 +210,32 @@ mod unsafe_libopus {
             frame_size: i32,
             decode_fec: i32,
         ) -> i32 {
-            let data = std::slice::from_raw_parts(data, len as _);
+            let data = std::slice::from_raw_parts(data, len as usize);
             let pcm = std::slice::from_raw_parts_mut(pcm, (frame_size * st.channels()) as usize);
-            opus_decode(st, data, pcm, frame_size, decode_fec)
+            st.decode(data, pcm, frame_size, decode_fec != 0)
         }
 
         unsafe fn opus_decoder_ctl_impl(
             st: &mut Box<OpusDecoder>,
             request: i32,
-            args: VarArgs,
+            mut args: VarArgs,
         ) -> i32 {
-            opus_decoder_ctl_impl(st, request, args)
+            match request {
+                OPUS_GET_FINAL_RANGE_REQUEST => {
+                    if let [VarArg::U32Out(ptr)] = &mut args.0[..] {
+                        **ptr = st.final_range();
+                        0
+                    } else {
+                        -1
+                    }
+                }
+                OPUS_RESET_STATE => {
+                    st.reset();
+                    0
+                }
+                // Fall back to the unsafe impl for unknown requests
+                other => ::unsafe_libopus::opus_decoder_ctl_impl(st, other, args),
+            }
         }
 
         unsafe fn opus_decoder_destroy(st: Box<OpusDecoder>) {
