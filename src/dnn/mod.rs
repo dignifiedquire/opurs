5dfedfda4f27d9286b52b9cf2ee03e9a1fff758f
dignifiedquire <me@dignifiedquire.com>
2026-03-03 13:39:11 +0100

    cleanup: drop module-level unused_assignments allow in dnn

--- src/dnn/mod.rs
+++ src/dnn/mod.rs
@@ -8,7 +8,6 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 #![allow(non_upper_case_globals)]
-#![allow(unused_assignments)]
 #![allow(clippy::too_many_arguments)]
 #![allow(clippy::excessive_precision)]
 #![allow(clippy::needless_range_loop)]
